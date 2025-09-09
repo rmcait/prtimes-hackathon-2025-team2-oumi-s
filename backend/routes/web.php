b53f5e536fb2f462aec856a29f52248e47579962
KOU050223 <kouhei20050223@icloud.com>
2025-09-09 22:58:51 +0900

    feat: なぜなぜ分析APIのルートを追加

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -10,3 +10,8 @@ Route::get('/', function () {
 Route::get('/strength-analyzer', function () {
     return view('strength-analyzer');
 })->name('strength-analyzer');
+
+// なぜなぜ分析チャットbot
+Route::get('/why-analyzer', function () {
+    return view('why-analyzer');
+})->name('why-analyzer');
