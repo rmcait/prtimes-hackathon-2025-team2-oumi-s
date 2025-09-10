99ed2af376224233d8f25ef51522b0d299745ac8
KOU050223 <kouhei20050223@icloud.com>
2025-09-10 12:09:52 +0900

    feat: ルートを修正し、コメントレビューを表示する

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReviewController;
 
-Route::get('/', [ReviewController::class, 'index']);
+// Route::get('/', [ReviewController::class, 'index']);
+Route::get('/', function () {
+    return view('comment-review');
+});
 Route::get('/review', [ReviewController::class, 'index']);
 Route::post('/proofread', [ReviewController::class, 'proofread'])->name('proofread');
 
